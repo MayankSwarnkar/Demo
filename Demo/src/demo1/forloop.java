a9cf205387265785d5ae486b761584758b67afea
user <user@DESKTOP-8ML0SI3>
2024-07-29 21:07:23 +0530

    Initial Commit.

---
+++ Demo/src/demo1/forloop.java
@@ -0,0 +1,15 @@
+package demo1;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=6; i++) {
+			for(int k = 1; k<=i; k++) {
+				System.out.print(" *");
+				}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Demo/src/demo1/jmjfat.java
@@ -0,0 +1,15 @@
+package demo1;
+
+public class jmjfat {
+
+	public static void main(String[] args) {
+		int i; int k;
+        for(i=1;i<=5;i++){
+            for(k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+	}
+	}
